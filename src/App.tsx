c82691ba67e8318d18273d035574896b9c759745
Leejinuk <leejin7900@gmail.com>
2024-06-01 11:20:08 +0900

    Feat: Vercel 연동테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
 import "./globals.css";
 import Home from "./pages/Home";
+import { Review } from "./types/Review";
 
 function App() {
+  const [data, setData] = useState<Review[]>([]);
+
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    fetch(`${apiUrl}/camera`)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div className="App">
+      <h1>Camera Reviews</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.content}</p>
+          </li>
+        ))}
+      </ul>
+
       <Home />
     </div>
   );
